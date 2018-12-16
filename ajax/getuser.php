f29d16e8b489c72547502dc58f698bd32efeb160
Yining He <1191401005@qq.com>
2018-12-16 23:18:28 +0100

    first try

--- ajax/getuser.php
+++ ajax/getuser.php
@@ -5,3 +5,33 @@
  * Date: 2018/12/12
  * Time: 22:59
  */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        table {
+            width:100%;
+            border-collapse  : collapse ;
+        }
+
+        table , td , th{
+            border: 1px solid black ;
+            padding : 5px;
+
+        }
+
+        th {text-align : left ; }
+    </style>
+</head>
+<body>
+<?php
+    $q = intval($_GERT['q']);
+
+
+?>
+
+</body>
+</html>
